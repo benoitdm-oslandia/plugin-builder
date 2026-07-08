a82cdaf14d60883aa7d36825cbfcca372d88640a
bdm-oslandia <benoit.de.mezzo@oslandia.com>
2026-07-08 17:04:13 +0200

    update ci

--- .github/build.cmd
+++ .github/build.cmd
@@ -6,26 +6,25 @@ echo PATH: %PATH%
 
 set "SEARCH_DIR=c:\Qt"
 set "FILE_NAME=Qt6Config.cmake"
-set "FOUND_FILE="
+set "QT6_PATH="
 
 echo Searching for %FILE_NAME% in %SEARCH_DIR%...
 for /f "delims=" %%i in ('dir /s /b "%SEARCH_DIR%\%FILE_NAME%" 2^>nul') do (
-    set "FOUND_FILE=%%~dpi"
+    set "QT6_PATH=%%~dpi"
     goto :found
 )
 
 :found
-if defined FOUND_FILE (
-    echo Found at: !FOUND_FILE!
-    set "CMAKE_MODULE_PATH=!CMAKE_MODULE_PATH!;!FOUND_FILE!"
+if defined QT6_PATH (
+    echo Qt6 found at: !QT6_PATH!
 ) else (
     echo File not found.
 )
 
-dir c:\osgeo4w\apps\qgis-ltr-dev
-
-
-set "CMAKE_MODULE_PATH=!CMAKE_MODULE_PATH!;c:\osgeo4w\apps\qgis-ltr-dev"
-echo CMAKE_MODULE_PATH: %CMAKE_MODULE_PATH%
-
-cmake -S . -B build -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-Wno-macro-redefined -DCMAKE_CXX_COMPILER=g++.exe -DCMAKE_C_COMPILER=gcc.exe
+cmake -S . -B build -GNinja ^
+-DCMAKE_BUILD_TYPE=Release ^
+-DCMAKE_CXX_FLAGS=-Wno-macro-redefined ^
+-DCMAKE_CXX_COMPILER=g++.exe ^
+-DCMAKE_C_COMPILER=gcc.exe ^
+-DQT6_PATH=!QT6_PATH! ^
+-DQGIS_PATH="c:\osgeo4w\apps\qgis-ltr-dev"

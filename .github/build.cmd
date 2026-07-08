ba7ced1a81de14ed202d2584bc946200e45006f8
bdm-oslandia <benoit.de.mezzo@oslandia.com>
2026-07-08 12:13:59 +0200

    update ci

--- .github/build.cmd
+++ .github/build.cmd
@@ -1,4 +1,8 @@
 set PATH=%PATH%;C:\ProgramData\chocolatey\bin;"C:\Program Files\CMake\bin"
 echo %PATH%
 
+dir c:\ProgramData\mingw64
+dir c:\ProgramData\mingw64\mingw64
+dir c:\ProgramData\mingw64\mingw64\bin
+
 cmake -S . -B build -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-Wno-macro-redefined -DCMAKE_CXX_COMPILER=c:\ProgramData\mingw64\mingw64\bin\g++ -DCMAKE_C_COMPILER=c:\ProgramData\mingw64\mingw64\bin\gcc

1fd72ef0838a654d35d2c89316a01781e7cacb04
bdm-oslandia <benoit.de.mezzo@oslandia.com>
2026-07-08 17:15:37 +0200

    update ci

--- .github/build.cmd
+++ .github/build.cmd
@@ -4,27 +4,28 @@ setlocal enabledelayedexpansion
 set "PATH=%PATH%;C:\ProgramData\chocolatey\bin;C:\Program Files\CMake\bin;c:\ProgramData\mingw64\mingw64\bin"
 echo PATH: %PATH%
 
-set "SEARCH_DIR=c:\Qt"
-set "FILE_NAME=Qt6Config.cmake"
-set "QT6_PATH="
+@REM set "SEARCH_DIR=c:\Qt"
+@REM set "FILE_NAME=Qt6Config.cmake"
+@REM set "QT6_PATH="
 
-echo Searching for %FILE_NAME% in %SEARCH_DIR%...
-for /f "delims=" %%i in ('dir /s /b "%SEARCH_DIR%\%FILE_NAME%" 2^>nul') do (
-    set "QT6_PATH=%%~dpi"
-    goto :found
-)
+@REM echo Searching for %FILE_NAME% in %SEARCH_DIR%...
+@REM for /f "delims=" %%i in ('dir /s /b "%SEARCH_DIR%\%FILE_NAME%" 2^>nul') do (
+@REM     set "QT6_PATH=%%~dpi"
+@REM     set "QT6_PATH=%QT6_PATH:\=/%"
+@REM     goto :found
+@REM )
 
-:found
-if defined QT6_PATH (
-    echo Qt6 found at: !QT6_PATH!
-) else (
-    echo File not found.
-)
+@REM :found
+@REM if defined QT6_PATH (
+@REM     echo Qt6 found at: !QT6_PATH!
+@REM ) else (
+@REM     echo File not found.
+@REM )
 
 cmake -S . -B build -GNinja ^
 -DCMAKE_BUILD_TYPE=Release ^
 -DCMAKE_CXX_FLAGS=-Wno-macro-redefined ^
 -DCMAKE_CXX_COMPILER=g++.exe ^
 -DCMAKE_C_COMPILER=gcc.exe ^
--DQT6_PATH=!QT6_PATH! ^
--DQGIS_PATH="c:\osgeo4w\apps\qgis-ltr-dev"
+-DQT6_PATH="C:/Qt/6.4.2/mingw_64/lib/cmake/Qt6" ^
+-DQGIS_PATH="c:/osgeo4w/apps/qgis-ltr-dev"

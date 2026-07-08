86f361310811826f0d424e2d756438396a4dfbe5
bdm-oslandia <benoit.de.mezzo@oslandia.com>
2026-07-08 11:59:46 +0200

    update ci

--- .github/build.cmd
+++ .github/build.cmd
@@ -1,3 +1,4 @@
-C:\ProgramData\chocolatey\bin\RefreshEnv.cmd
+set PATH=%PATH%;C:\ProgramData\chocolatey\bin;"C:\Program Files\CMake\bin"
+echo %PATH%
 
 cmake -S . -B build -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-Wno-macro-redefined

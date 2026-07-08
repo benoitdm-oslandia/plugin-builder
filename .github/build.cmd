0f6ef3b3e9d4a3845298684594d83cb79c2d0182
bdm-oslandia <benoit.de.mezzo@oslandia.com>
2026-07-08 12:16:36 +0200

    update ci

--- .github/build.cmd
+++ .github/build.cmd
@@ -6,4 +6,4 @@ dir c:\ProgramData\mingw64
 dir c:\ProgramData\mingw64\mingw64
 dir c:\ProgramData\mingw64\mingw64\bin
 
-cmake -S . -B build -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-Wno-macro-redefined -DCMAKE_CXX_COMPILER=c:\ProgramData\mingw64\mingw64\bin\g++ -DCMAKE_C_COMPILER=c:\ProgramData\mingw64\mingw64\bin\gcc
+cmake -S . -B build -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-Wno-macro-redefined -DCMAKE_CXX_COMPILER=c:\ProgramData\mingw64\mingw64\bin\g++.exe -DCMAKE_C_COMPILER=c:\ProgramData\mingw64\mingw64\bin\gcc.exe

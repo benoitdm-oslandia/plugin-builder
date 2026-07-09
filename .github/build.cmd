7e3358f9db460dab83766430ce0f2ea65a17d7cd
bdm-oslandia <benoit.de.mezzo@oslandia.com>
2026-07-09 10:14:37 +0200

    update ci (working cmake -S)
    update ci
    
    update ci
    
    update ci
    
    update ci

--- .github/build.cmd
+++ .github/build.cmd
@@ -1,34 +1,41 @@
-@echo off
-setlocal enabledelayedexpansion
+@REM echo off
+@REM setlocal enabledelayedexpansion
 
 set "PATH=%PATH%;C:\ProgramData\chocolatey\bin;C:\Program Files\CMake\bin;c:\ProgramData\mingw64\mingw64\bin"
 echo PATH: %PATH%
 
-@REM set "SEARCH_DIR=c:\Qt"
-@REM set "FILE_NAME=Qt6Config.cmake"
-@REM set "QT6_PATH="
 
-@REM echo Searching for %FILE_NAME% in %SEARCH_DIR%...
-@REM for /f "delims=" %%i in ('dir /s /b "%SEARCH_DIR%\%FILE_NAME%" 2^>nul') do (
-@REM     set "QT6_PATH=%%~dpi"
-@REM     set "QT6_PATH=%QT6_PATH:\=/%"
-@REM     goto :found
-@REM )
+@REM search Qt6 cmake file
 
-@REM :found
-@REM if defined QT6_PATH (
-@REM     echo Qt6 found at: %QT6_PATH%
-@REM ) else (
-@REM     echo File not found.
-@REM )
+set "SEARCH_DIR=c:\Qt"
+set "FILE_NAME=Qt6Config.cmake"
+set "QT6_PATH="
 
-dir
-echo "copy FindQGIS.cmake %OSGEO4W_ROOT%/apps/%OSGEO4W_QGIS_SUBDIR%/"
+echo Searching for %FILE_NAME% in %SEARCH_DIR%...
+for /f "delims=" %%i in ('dir /s /b "%SEARCH_DIR%\%FILE_NAME%" 2^>nul') do (
+    set "QT6_PATH=%%~dpi"
+    goto :found
+)
+
+:found
+if defined QT6_PATH (
+    echo Qt6 found at: %QT6_PATH%
+) else (
+    echo File not found.
+)
+
+@REM use " to transform path separator from / to \ in copy cmd
+copy FindQGIS.cmake "%OSGEO4W_ROOT%/apps/%OSGEO4W_QGIS_SUBDIR%/"
+
+@REM OSGEO4W_ROOT, QT6_PATH and QGIS_PATH must not use \ path separator
+set "QT6_PATH_UNIX=%QT6_PATH:\=/%"
 
 cmake -S . -B build -GNinja ^
 -DCMAKE_BUILD_TYPE=Release ^
 -DCMAKE_CXX_FLAGS=-Wno-macro-redefined ^
 -DCMAKE_CXX_COMPILER=g++.exe ^
 -DCMAKE_C_COMPILER=gcc.exe ^
--DQT6_PATH="C:/Qt/6.4.2/mingw_64/lib/cmake/Qt6" ^
+-DQT6_PATH="%QT6_PATH_UNIX%" ^
 -DQGIS_PATH="%OSGEO4W_ROOT%/apps/%OSGEO4W_QGIS_SUBDIR%"
+
+cmake --build build
